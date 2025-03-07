3905426da17a22d53d7bc38b73d305e35ff9c757
Leonardo Guarnieri de Bastiani <leogbastiani@gmail.com>
2025-03-07 09:10:40 -0300

    group variables

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ import { parse, TYPE } from "@formatjs/icu-messageformat-parser";
   //   leo: 'Convite de {therapistName}, para o dia {date, date} {number, number} {time, time}, <b>às</b> <b>{leo}</b> {hour}h{minute, select, 0 {# leo} other {<d>{minute}</d>}}. {plural, plural, one {# pessoa} other {# pessoas}}',
   // };
 
-  function getValues(str) {
+  function parseAndGetValues(str) {
     if (typeof str !== "string") {
       return [];
     }
@@ -51,85 +51,110 @@ import { parse, TYPE } from "@formatjs/icu-messageformat-parser";
     return variables;
   }
 
-  const crowdin = Object.entries(translations).map(([id, value]) => ({
-    id,
-    values: getValues(value),
-  }));
+  function toTypeString(type) {
+    if (type === TYPE.date) {
+      return "Date";
+    }
+    if (type === TYPE.time) {
+      return "Date";
+    }
+    if (type === TYPE.number || type === TYPE.plural) {
+      return "number";
+    }
+    if (type === TYPE.tag) {
+      return "(chunks: string) => Node";
+    }
+    return "string | number";
+  }
 
   function valuesToString(values) {
     return `{${values
       .map(
         ({ name, type }) =>
-          `${name}${name === "b" && type === TYPE.tag ? "?" : ""}: ${(() => {
-            if (type === TYPE.date) {
-              return "Date";
-            }
-            if (type === TYPE.time) {
-              return "Date";
-            }
-            if (type === TYPE.number || type === TYPE.plural) {
-              return "number";
-            }
-            if (type === TYPE.tag) {
-              return "(chunks: string) => Node";
-            }
-            return "string | number";
-          })()}`
+          `${name}${
+            name === "b" && type === TYPE.tag ? "?" : ""
+          }: ${toTypeString(type)}`
       )
       .join("; ")}}`;
   }
 
-  function questionMarkInValues(values) {
-    if (
-      values.length === 1 &&
-      values[0].type === TYPE.tag &&
-      values[0].name === "b"
-    ) {
-      return "?";
-    }
-    return "";
-  }
+  const crowdin = Object.entries(translations)
+    // .filter(([id]) => id === "EXAMPLE")
+    .map(([id, crowdinValue]) => {
+      const values = parseAndGetValues(crowdinValue);
+      const groupedValues = {};
+      for (const value of values) {
+        groupedValues[value.name] ??= new Set();
+        groupedValues[value.name].add(toTypeString(value.type));
+      }
+      const groupedValuesString = Object.fromEntries(
+        Object.entries(groupedValues).map(([key, value]) => {
+          return [
+            key,
+            `${
+              value.size === 1
+                ? value.values().next().value
+                : `(${[...value].map((x) => `(${x})`).join(" & ")})`
+            }`,
+          ];
+        })
+      );
+      const isOptional =
+        Object.values(groupedValuesString).length === 1 &&
+        groupedValuesString.b === toTypeString(TYPE.tag);
+      const outputType =
+        Object.entries(groupedValuesString).length === 0
+          ? ""
+          : `{${Object.entries(groupedValuesString)
+              .map(([key, value]) => {
+                const isOptional =
+                  key === "b" && value === toTypeString(TYPE.tag);
+                return `${key}${isOptional ? "?" : ""}: ${value}`;
+              })
+              .join(";")}}`;
+      return {
+        id,
+        isOptional,
+        outputType,
+      };
+    });
 
   console.log(`/* eslint-disable */
 
-  // enable me in tsconfig, uncomment "react-intl": ["./react-intl-custom"]
-  import * as React from 'react';
+// enable me in tsconfig, uncomment "react-intl": ["./react-intl-custom"]
+import * as React from 'react';
 
-  import { IntlFormatters, IntlShape as IntlShapeOrig } from 'react-intl-orig';
+import { IntlFormatters, IntlShape as IntlShapeOrig } from 'react-intl-orig';
 
-  export * from 'react-intl-orig';
+export * from 'react-intl-orig';
 
-  type Crowdin<Node = string | number> = ${crowdin
+type Crowdin<Node = string | number> = ${crowdin
     .map(
-      ({ id, values }) =>
+      ({ id, isOptional, outputType }) =>
         `{id:'${id}'${
-          values.length === 0
-            ? ""
-            : `, values${questionMarkInValues(values)}: ${valuesToString(
-                values
-              )}`
+          outputType && `, values${isOptional ? "?" : ""}: ${outputType}`
         }}`
     )
     .join("|")};
-  export const FormattedMessage: React.ComponentType<Crowdin<React.ReactNode> & {
-    children?(nodes: React.ReactNode[]): React.ReactElement | null;
-  }>;
+export const FormattedMessage: React.ComponentType<Crowdin<React.ReactNode> & {
+children?(nodes: React.ReactNode[]): React.ReactElement | null;
+}>;
 
-  type ApplyValuesOptional<T extends { id: string }> = T extends {
-    values?: any;
-  }
-    ? undefined extends T['values']
-      ? [id: { id: T['id'] }, values?: T['values']]
-      : [id: { id: T['id'] }, values: T['values']]
-    : [id: { id: T['id'] }];
+type ApplyValuesOptional<T extends { id: string }> = T extends {
+values?: any;
+}
+? undefined extends T['values']
+    ? [id: { id: T['id'] }, values?: T['values']]
+    : [id: { id: T['id'] }, values: T['values']]
+: [id: { id: T['id'] }];
 
-  function formatMessage<const T extends Crowdin['id']>(
-    ...args: ApplyValuesOptional<Extract<Crowdin, { id: T }>>
-  ): string;
+function formatMessage<const T extends Crowdin['id']>(
+...args: ApplyValuesOptional<Extract<Crowdin, { id: T }>>
+): string;
 
-  export type IntlShape = Omit<IntlShapeOrig, keyof IntlFormatters> & {
-    formatMessage: typeof formatMessage;
-  };
-  export function useIntl(): IntlShape;
-  `);
+export type IntlShape = Omit<IntlShapeOrig, keyof IntlFormatters> & {
+formatMessage: typeof formatMessage;
+};
+export function useIntl(): IntlShape;
+`);
 })();
